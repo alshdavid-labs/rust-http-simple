d48090f733565fa2f4255fb61687792d5a75ea4a
David Alsh <alshdavid@gmail.com>
2024-03-28 17:41:55 +1100

    sync

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
-use std::net::IpAddr;
-use std::str::FromStr;
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
-use rocket::config::LogLevel;
-use rocket::get;
-use rocket::launch;
-use rocket::post;
-use rocket::routes;
-use rocket::Config;
+async fn main_async() {
+    let listener = TcpListener::bind("0.0.0.0:9000").unwrap();
+    println!("Listening for connections on port {}", 8080);
 
-#[get("/")]
-fn index_get() -> String {
-    return "Hello, world!".to_string();
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+
+        let mut headers = [httparse::EMPTY_HEADER; 64];
+        let mut request = httparse::Request::new(&mut headers);
+
+        let status = request.parse(buffer.as_slice()).unwrap().unwrap();
+        let body = &buffer[status..];
+        let contents = std::str::from_utf8(body).unwrap();
+
+        let status_line = "HTTP/1.1 200 OK";
+        let length = contents.len();
+
+        let response =
+            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
 
-#[post("/", data = "<body>")]
-fn index_post(body: String) -> String {
-    return body;
-}
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .configure(Config {
-            address: IpAddr::from_str("0.0.0.0").unwrap(),
-            port: 9000,
-            // workers: 4,
-            workers: Default::default(),
-            log_level: LogLevel::Off,
-            ..Config::default()
-        })
-        .mount("/", routes![index_get, index_post,])
+fn main() {
+  tokio::runtime::Builder::new_multi_thread()
+    .worker_threads(num_cpus::get())
+    // .worker_threads(threads as usize)
+    .enable_all()
+    .build()
+    .unwrap()
+    .block_on(main_async());
 }
